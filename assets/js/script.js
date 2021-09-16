d8b022d2c3796ebb572dbe879ace4923d5e0743b
Anthony <anthony.ringeisen.dev@gmail.com>
2021-09-16 10:24:50 +0200

    calculatrice terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 const buttons = document.querySelectorAll('.btn');
+const result = document.getElementById('result')
 
-console.log(buttons);
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        result.textContent += e.target.id;
+    });
+});
+
+equal.addEventListener('click', () => {
+    result.textContent = eval(result.textContent); 
+});
+
+clear.addEventListener('click', () => {
+    result.textContent = "";
+})
+
+// -----------------------
+
